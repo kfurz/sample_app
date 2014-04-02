9375cf891f6d5e84e4024467ebde8fd758ee6ffa
Keifer Furzland <furzland@gmail.com>
2014-04-02 01:27:03 -0500

    adds titles to pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,30 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be succesful" do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      visit '/pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
+
 
   describe "GET 'contact'" do
     it "should be succesful" do
       get 'contact'
       response.should be_success
+    end
+
+      it "should have the right title" do
+      visit '/pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
@@ -21,6 +33,10 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-  end
 
+      it "should have the right title" do
+      visit '/pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
 end
