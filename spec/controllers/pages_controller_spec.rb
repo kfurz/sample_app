7d805203817ca4eb80a0bc832f8e0e25303f33c6
Keifer Furzland <furzland@gmail.com>
2014-04-02 15:44:34 -0500

    added layout and help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 
   describe "GET 'home'" do
     it "should be succesful" do
@@ -11,10 +13,14 @@ describe PagesController do
 
     it "should have the right title" do
       visit '/pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
+    end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
-
 
   describe "GET 'contact'" do
     it "should be succesful" do
@@ -24,7 +30,7 @@ describe PagesController do
 
       it "should have the right title" do
       visit '/pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 
@@ -33,10 +39,23 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-
+ 
       it "should have the right title" do
       visit '/pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
+
+  describe "GET 'help'" do
+    it "should be succesful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+    visit '/pages/help'
+    expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
 end
